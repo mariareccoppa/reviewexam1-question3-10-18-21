8d760209a2f04435de170f9a2c24c1c1ebe7015c
Maria Reccoppa <83091797+mariareccoppa@users.noreply.github.com>
2021-10-18 16:03:40 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,6 @@
+let m = parseFloat(prompt("Please type a number"))
+let n = parseFloat(prompt("Please type another number"))
+
+if (m*n >0) console.log(1)
+if (m*n ==0) console.log(0)
+if (m*n <0) console.log(-1)
